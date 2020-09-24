d2666ecc9d684c074d6ba6731a2fe542ca8d4961
Or-Dai <1124667200@qq.com>
2020-09-24 11:54:06 +0800

    用户类修改

--- src/main/java/com/example/springboot/entity/User.java
+++ src/main/java/com/example/springboot/entity/User.java
@@ -6,14 +6,29 @@ import javax.persistence.*;
 @Table(name = "user")
 public class User {
     @Id
-    @GeneratedValue
-    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
-    @Column(name="username" )
+    @Column
     private String username;
-    @Column(name="password")
+    @Column
     private String password;
+    @Column
+    private String alias;
+    @Column
+    private String name;
+    @Column
+    private String gender;
+    @Column
+    private String email;
+    @Column
+    private String tel ;
+    @Column(name="delivery_address")
+    private String deliveryAddress;
+    @Column(name="payment_way")
+    private Integer paymentWay ;
+    @Column(name="images_url")
+    private String imagesUrl;
 
     public Integer getId() {
         return id;
@@ -37,5 +52,70 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+
+    public Integer getPaymentWay() {
+        return paymentWay;
+    }
+
+    public void setPaymentWay(Integer paymentWay) {
+        this.paymentWay = paymentWay;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public String getImagesUrl() {
+        return imagesUrl;
+    }
+
+    public void setImagesUrl(String imagesUrl) {
+        this.imagesUrl = imagesUrl;
     }
 }

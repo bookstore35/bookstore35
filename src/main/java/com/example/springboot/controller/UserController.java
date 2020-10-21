24b840c7e0e03195a2c8562c8205ec14641589fe
ame <1124667200@qq.com>
2020-10-21 15:31:52 +0800

    新增：User修改、删除接口

--- src/main/java/com/example/springboot/controller/UserController.java
+++ src/main/java/com/example/springboot/controller/UserController.java
@@ -14,6 +14,12 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
+/**
+ * 用户管理
+ *
+ * @author yife
+ * @return
+ */
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -22,19 +28,26 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-
-    @GetMapping("/findAll")
-    public Result<User> findAll(){
-        return Result.success(userService.findAll());
+    /**
+     * 登录接口
+     * @param username  用户名
+     * @param password  密码
+     * @return
+     *
+     */
+    @GetMapping("/login")
+    public Result<User>  login(String username , String password , HttpServletRequest request){
+        return userService.login(username ,password);
     }
+
     /**
      * 注册接口
      * @param user
      * @return
      */
-    @PostMapping
-    @RequestMapping("/regist")
+    @PostMapping("/regist")
     public Result<User>  regist(@RequestBody User user,HttpServletRequest request){
+
         try {
             if (!codeUtil.checkVerifyCode(request)) {
                 return Result.error("验证码出错");
@@ -45,72 +58,69 @@ public class UserController {
         }
         return this.userService. regist(user);
     }
-    /*修改用户信息
-    * */
-    @PostMapping
-    @RequestMapping("/update")
+    /**
+     * 查询所有用户接口
+     * @return
+     */
+    @GetMapping("/findAll")
+    public Result<User> findAll(){
+        return Result.success(userService.findAll());
+    }
+    /**
+     * 根据id查询用户接口
+     *
+     * @return
+     */
+    @UserLoginToken
+    @GetMapping("/getById")
+    public Result<User>  getById(Integer id){
+
+        return userService.getById(id);
+    }
+    /**
+     * 修改用户接口
+     * @param user
+     * @return
+     */
+    @PostMapping("/update")
     public Result<User> update(@RequestBody User user ){
 
         return this.userService.update(user);
 }
 
-    /*删除接口
-    * */
-    @DeleteMapping
-    @RequestMapping("/delete/{id}")
+    /**
+     * 删除用户接口
+     *
+     * @return
+     */
+    @DeleteMapping("/delete/{id}")
     public Result<User> delete(@PathVariable("id") Integer id ){
         return this.userService.delete(id);
     }
 
-    /*根据id查询用户
-    * */
-    @UserLoginToken
-    @GetMapping
-    @RequestMapping("/getById")
-    public Result<User>  getById(Integer id){
-
-        return userService.getById(id);
-    }
-
-
-
-    @GetMapping
-    @RequestMapping("/login")
-    public Result<User>  login(String username , String password , HttpServletRequest request){
-  /*      try {
-            if (!codeUtil.checkVerifyCode(request)) {
-                return Result.error("验证码出错");
-            }
-        } catch (Exception e) {
-            // 返回错误信息给前端
-            return Result.error(e.getMessage());
-        }*/
-        return userService.login(username ,password);
-    }
-    @GetMapping
-    @RequestMapping("/loginVerify")
+    /**
+     * 手机登录发送验证码接口
+     *
+     * @return
+     */
+    @GetMapping("/loginVerify")
     public Result<User>  loginVerify(String moblie , String code , HttpServletRequest request){
 
         return userService.loginVerify(moblie ,code);
     }
 
     /**
-     *
+     * 发送短信验证码接口
      * @param tel 手机号
      * @param type 短信类型：0 注册， 1 登录
      * @return
      */
-    @GetMapping
-    @RequestMapping("/send")
+    @GetMapping("/send")
     public Result sendMessage(String tel , Integer type){
 
         //type_tel :key
         return userService.sendMessage(tel,type);
     }
-
-    /*
-    *
-    * */
 
 
 }
